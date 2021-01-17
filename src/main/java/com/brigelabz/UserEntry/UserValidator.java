6dfe76dbf52e7e0ef2f91d50e7b6c53d19a56eed
GURUMANCHI-SAI-KALYAN <kalyangurumanchi@gmail.com>
2021-01-17 15:45:18 +0530

    ADDED : ENTER LASTNAME VALIDATION PROGRAM

--- src/main/java/com/brigelabz/UserEntry/UserValidator.java
+++ src/main/java/com/brigelabz/UserEntry/UserValidator.java
@@ -2,10 +2,14 @@ package com.brigelabz.UserEntry;
 import java.util.regex.Pattern;
 
 public class UserValidator {
-    private static final String First_Name_Pattern = "^[A-Z]{1}[a-z]{2,}$";
+    private static final String Name_Pattern = "^[A-Z]{1}[a-z]{2,}$";
 
     public boolean validateFirstName(String fname) {
-        Pattern pattern = Pattern.compile(First_Name_Pattern);
+        Pattern pattern = Pattern.compile(Name_Pattern);
         return pattern.matcher(fname).matches();
+    }
+    public boolean validateLastName(String lname) {
+        Pattern pattern = Pattern.compile(Name_Pattern);
+        return pattern.matcher(lname).matches();
     }
 }
--- src/test/java/com/brigelabz/UserEntry/UserValidatorTest.java
+++ src/test/java/com/brigelabz/UserEntry/UserValidatorTest.java
@@ -8,7 +8,13 @@ public class UserValidatorTest {
     @Test
     public void givenFirstName_WhenProper_ShouldReturnTrue() {
         UserValidator validator = new UserValidator();
-        boolean result = validator.validateFirstName("Kalyan");
+        boolean result = validator.validateFirstName("Sai");
+        Assert.assertTrue(result);
+    }
+    @Test
+    public void givenLastName_WhenProper_ShouldReturnTrue() {
+        UserValidator validator = new UserValidator();
+        boolean result = validator.validateLastName("Kalyan");
         Assert.assertTrue(result);
     }
 }
