29bbcaec8d87e33654fae7a19d8c3c7557879d37
GURUMANCHI-SAI-KALYAN <kalyangurumanchi@gmail.com>
2021-01-22 21:34:17 +0530

    ADDED : RETURN COUSTEM EXCEPTIONS IN CASE OF FALSE PROGRAM

--- src/main/java/com/brigelabz/UserEntry/UserValidator.java
+++ src/main/java/com/brigelabz/UserEntry/UserValidator.java
@@ -1,5 +1,6 @@
 package com.brigelabz.UserEntry;
 import java.util.regex.Pattern;
+//welcome
 
 public class UserValidator {
     private static final String Name_Pattern = "^[A-Z]{1}[a-z]{2,}$";
@@ -7,24 +8,29 @@ public class UserValidator {
     private static final String Phone_Number_Pattern = "^91[6-9]{1}[0-9]{9}$";
     private static final String Password_Pattern = "^(?=.*[A-Z])(?=,*[0-9]) [a-zA-Z0-9]{8,}$";
 
-    public boolean validateFirstName(String fname) {
+    public boolean validateFirstName(String fname) throws UserRegistrationExceptions {
         Pattern pattern = Pattern.compile(Name_Pattern);
-        return pattern.matcher(fname).matches();
+        if(pattern.matcher(fname).matches())return true;
+        throw new UserRegistrationExceptions("please Enter Proper firstname");
     }
-    public boolean validateLastName(String lname) {
+    public boolean validateLastName(String lname) throws UserRegistrationExceptions {
         Pattern pattern = Pattern.compile(Name_Pattern);
-        return pattern.matcher(lname).matches();
+        if(pattern.matcher(lname).matches())return true;
+        throw new UserRegistrationExceptions("please Enter Proper lastname");
     }
-    public boolean validateEmail(String Email) {
+    public boolean validateEmail(String Email) throws UserRegistrationExceptions {
         Pattern pattern = Pattern.compile(Email_Pattern);
-        return pattern.matcher(Email).matches();
+        if(pattern.matcher(Email).matches())return true;
+        throw new UserRegistrationExceptions("please Enter Proper Email");
     }
-    public boolean validatePhoneNumber(String phoneNumber) {
+    public boolean validatePhoneNumber(String phoneNumber) throws UserRegistrationExceptions {
         Pattern pattern = Pattern.compile(Phone_Number_Pattern);
-        return pattern.matcher(phoneNumber).matches();
+        if(pattern.matcher(phoneNumber).matches())return true;
+        throw new UserRegistrationExceptions("please Enter Proper phoenumber");
     }
-    public boolean validatePassword(String password) {
+    public boolean validatePassword(String password) throws UserRegistrationExceptions {
         Pattern pattern = Pattern.compile(Password_Pattern);
-        return pattern.matcher(password).matches();
+        if(pattern.matcher(password).matches())return true;
+        throw new UserRegistrationExceptions("please Enter Proper password");
     }
 }
